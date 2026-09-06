21853c131b1a1b5f567107b3dd88b7242d567307
Ron Jayson Gallardo <ronjaysonlapitangallardo@gmail.com>
2026-09-07 03:08:12 +0800

    Gradient Keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,15 @@ module.exports = {
         'fade-slide-up': {
           '0%': { opacity: '0', transform: 'translateY(20px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        'gradient-shift': {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
         }
       },
       animation: {
         'fade-slide-up': 'fade-slide-up 0.6s ease-out forwards',
+        'gradient-shift': 'gradient-shift 8s ease infinite',
       }
     },
   },
