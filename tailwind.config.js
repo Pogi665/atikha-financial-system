d5e8a82ac82e60c470ac25123b32c4dfc1c6d3c0
Ron Jayson Gallardo <ronjaysonlapitangallardo@gmail.com>
2026-09-07 02:46:03 +0800

    Custom Keyframes and Animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./**/*.php"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-slide-up': {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        'fade-slide-up': 'fade-slide-up 0.6s ease-out forwards',
+      }
+    },
   },
   plugins: [],
 }
